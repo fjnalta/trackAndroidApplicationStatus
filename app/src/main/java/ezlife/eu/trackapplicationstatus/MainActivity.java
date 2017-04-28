47dd94b3d8e7b06fe4d359358f7e1054ff573a65
Philipp Minges <fjnalta@gmail.com>
2017-04-28 23:21:44 +0200

    2

--- app/src/main/java/ezlife/eu/trackapplicationstatus/MainActivity.java
+++ app/src/main/java/ezlife/eu/trackapplicationstatus/MainActivity.java
@@ -2,11 +2,9 @@ package ezlife.eu.trackapplicationstatus;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 /**
  * Created by ajo on 28.04.17.
